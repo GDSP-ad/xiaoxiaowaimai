8b8a3a9118923f4a1501cf221490ada70eba53ac
AD <2489297748@qq.com>
2023-04-24 10:02:02 +0800

    添加了less

--- xiaoxiaowaimai/src/App.vue
+++ xiaoxiaowaimai/src/App.vue
@@ -3,16 +3,9 @@
 </script>
 
 <template>
-  <div>
-    123
-  </div>
   <router-view></router-view>
 </template>
 
 <style scoped>
-  div {
-    background-color: red;
-    width: 5rem;
-    height: 5rem;
-  }
+
 </style>
--- xiaoxiaowaimai/src/views/home/Home.vue
+++ xiaoxiaowaimai/src/views/home/Home.vue
@@ -1,3 +1,10 @@
 <template>
     <div>首页</div>
 </template>
+
+<style lang="less" scoped>
+div {
+    background-color: aqua;
+}
+
+</style>
