8281d06a333ada4ed87e8c20dea7c89e82bfd2f8
AD <2489297748@qq.com>
2023-04-24 08:40:04 +0800

    123

--- xiaoxiaowaimai/vite.config.ts
+++ xiaoxiaowaimai/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import postCssPxToRem from "postcss-pxtorem";
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    postCssPxToRem({
+      rootValue: 75, // （设计稿/10）1rem的大小
+      propList: ['*'], // 需要转换的属性，这里选择全部都进行转换
+  })
+  ],
 })
