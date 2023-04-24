48ad2223b7e9c202e4ee8bd8e1ff922b16224d15
AD <2489297748@qq.com>
2023-04-24 16:22:56 +0800

    添加了首页大分类

--- xiaoxiaowaimai/src/views/home/Home.vue
+++ xiaoxiaowaimai/src/views/home/Home.vue
@@ -21,7 +21,14 @@
                 </div>
 
                 <div class="sort">
-                    <div class="big-sort"></div>
+                    <div class="big-sort">
+                        <div v-for="(item,index) in data.big_sort" :key="index">
+                            <svg class="icon" aria-hidden="true">
+                                <use :xlink:href="`#${item.icon}`"></use>
+                            </svg>
+                            {{ item.name }}
+                        </div>
+                    </div>
                     <div class="small-sort"></div>
                 </div>
             </div>
@@ -33,7 +40,160 @@
 
 
 <script setup lang="ts">
+import { reactive } from 'vue';
 import Footer from '../../components/Footer.vue';
+
+const data = reactive({
+    big_sort: [
+        {
+            name: "美食",
+            icon: "icon-meishi",
+        },
+        {
+            name: "甜点饮品",
+            icon: "icon-tiandian",
+        },
+        {
+            name: "超市便利",
+            icon: "icon-chaoshigouwu",
+        },
+        {
+            name: "生鲜果蔬",
+            icon: "icon-shengxian-ganju",
+        },
+        {
+            name: "买药",
+            icon: "icon-zhichiyiyuanyaodian",
+        },
+    ],
+    small_sort: [
+        {
+            name: "午餐",
+            icon: "icon-wucan",
+        },
+        {
+            name: "买酒",
+            icon: "icon-jiu",
+        },
+        {
+            name: "新鲜水果",
+            icon: "icon-xinxianguoshu_xinxianshuiguopingguo",
+        },
+        {
+            name: "汉堡披萨",
+            icon: "icon-hanbaopisa",
+        },
+        {
+            name: "休闲饮品",
+            icon: "icon-xiuxianyinpin",
+        },
+        {
+            name: "夜宵",
+            icon: "icon-yexiao",
+        },
+        {
+            name: "吐司",
+            icon: "icon-tusi",
+        },
+        {
+            name: "跑腿",
+            icon: "icon-paotuiAPP",
+        },
+        {
+            name: "美人佳丽",
+            icon: "icon-kouhong",
+        },
+        {
+            name: "全部分类",
+            icon: "icon-fenlei",
+        },
+    ],
+    centent_nav_list: [
+        {
+            tab: "天天神券",
+            data: [
+                {
+                    pic: "https://img1.baidu.com/it/u=1599947592,1695977044&fm=253&fmt=auto&app=138&f=JPEG?w=640&h=440",
+                    title: "酸菜鱼",
+                    sales: "2888",
+                    price: "20",
+                    label: ["门店上新", "很下饭"],
+                },
+                {
+                    pic: "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fwww.cfcy168.com%2FUploadFiles%2F2020%2F2%2F15904074889874037.jpg&refer=http%3A%2F%2Fwww.cfcy168.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1645421933&t=66b58fbba9dce6f6b397e38820de24dc",
+                    title: "隆江猪脚饭",
+                    sales: "2888",
+                    price: "20",
+                    label: ["门店上新", "很下饭"],
+                },
+                {
+                    pic: "https://img1.baidu.com/it/u=1599947592,1695977044&fm=253&fmt=auto&app=138&f=JPEG?w=640&h=440",
+                    title: "酸菜鱼",
+                    sales: "2888",
+                    price: "20",
+                    label: ["门店上新", "很下饭"],
+                },
+                {
+                    pic: "https://img1.baidu.com/it/u=1599947592,1695977044&fm=253&fmt=auto&app=138&f=JPEG?w=640&h=440",
+                    title: "酸菜鱼",
+                    sales: "2888",
+                    price: "20",
+                    label: ["门店上新", "很下饭"],
+                },
+            ],
+        },
+        {
+            tab: "减配送费",
+            data: [
+                {
+                    pic: "https://img1.baidu.com/it/u=1599947592,1695977044&fm=253&fmt=auto&app=138&f=JPEG?w=640&h=440",
+                    title: "酸菜鱼",
+                    sales: "2888",
+                    price: "20",
+                    label: ["门店上新", "很下饭"],
+                },
+            ],
+        },
+        {
+            tab: "点评高分",
+            data: [
+                {
+                    pic: "https://img1.baidu.com/it/u=1599947592,1695977044&fm=253&fmt=auto&app=138&f=JPEG?w=640&h=440",
+                    title: "酸菜鱼",
+                    sales: "2888",
+                    price: "20",
+                    label: ["门店上新", "很下饭"],
+                },
+                {
+                    pic: "https://img1.baidu.com/it/u=1599947592,1695977044&fm=253&fmt=auto&app=138&f=JPEG?w=640&h=440",
+                    title: "酸菜鱼",
+                    sales: "2888",
+                    price: "20",
+                    label: ["门店上新", "很下饭"],
+                },
+            ],
+        },
+        {
+            tab: "会员满减",
+            data: [
+                {
+                    pic: "https://img1.baidu.com/it/u=1599947592,1695977044&fm=253&fmt=auto&app=138&f=JPEG?w=640&h=440",
+                    title: "酸菜鱼",
+                    sales: "2888",
+                    price: "20",
+                    label: ["门店上新", "很下饭"],
+                },
+                {
+                    pic: "https://img1.baidu.com/it/u=1599947592,1695977044&fm=253&fmt=auto&app=138&f=JPEG?w=640&h=440",
+                    title: "酸菜鱼",
+                    sales: "2888",
+                    price: "20",
+                    label: ["门店上新", "很下饭"],
+                },
+            ],
+        },
+    ],
+})
 </script>
 
 <style lang="less" scoped>
@@ -46,18 +206,22 @@ import Footer from '../../components/Footer.vue';
     .content {
         flex: 1;
         overflow-y: auto;
+
         .header {
-            background-image: linear-gradient(#fff400,#fff);
+            background-image: linear-gradient(#fff400, #fff);
             display: flex;
             justify-content: space-between;
             align-items: center;
             padding: 20px 20px 40px 20px;
+
             .text {
                 font-size: 20px;
                 font-weight: 600;
             }
+
             .location {
                 font-size: 14px;
+
                 span {
                     margin: 0 5px;
                 }
@@ -66,18 +230,22 @@ import Footer from '../../components/Footer.vue';
 
         .main {
             margin-top: -30px;
+
             .main-bg {
-                background-image: linear-gradient(#fff,#f5f5f5);
+                background-image: linear-gradient(#fff, #f5f5f5);
                 padding: 10px 20px 0 20px;
                 border-radius: 30px 30px 0 0;
+
                 .search {
                     position: relative;
+
                     input {
                         width: 100%;
                         border: 1px solid #ffc400;
                         border-radius: 20px;
                         height: 30px;
                     }
+
                     .search-text {
                         position: absolute;
                         right: -6px;
@@ -92,7 +260,27 @@ import Footer from '../../components/Footer.vue';
                     }
                 }
             }
+
+            .sort {
+                padding: 20px 0;
+                .big-sort {
+                    display: flex;
+
+                    div {
+                        display: flex;
+                        flex: 1;
+                        justify-content: center;
+                        flex-direction: column;
+                        align-items: center;
+                        .icon {
+                            width: 50px;
+                            height: 50px;
+                            margin-bottom: 5px;
+                        }
+                        font-size: 14px;
+                    }
+                }
+            }
         }
     }
-}
-</style>
+}</style>
