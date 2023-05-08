00df71087720065b74a72ae7c001ff2f53a365e8
AD <2489297748@qq.com>
2023-05-08 09:13:06 +0800

    修改了1rem为37.5px

--- xiaoxiaowaimai/postcss.config.cjs
+++ xiaoxiaowaimai/postcss.config.cjs
@@ -10,7 +10,7 @@ module.exports = {
       ]
     },
     'postcss-pxtorem': {
-      rootValue: 75,
+      rootValue: 37.5,
       //这是基准值，在375px的屏幕变大rem的值会变大，小于这个大小元素的rem值会变小
       propList: ['*']
     }
